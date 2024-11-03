b4224b75228e5635550026b77303878f0d9fe167
Risky <notriskysh@gmail.com>
2024-11-03 13:00:52 +0530

    added a bit responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import { Container } from "@/components/container";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="space-y-6 px-2 py-8 text-center lg:py-24 md:py-12 ">
+    <div className="mx-auto grid justify-center gap-4 md:max-w-[64rem] md:grid-cols-3 sm:grid-cols-2 ">
+      <Container url={"/"} title={"Test"} description={"This is just a testing text"} />
+      <Container url={"/"} title={"Test"} description={"This is just a testing text"} />
+      <Container url={"/"} title={"Test"} description={"This is just a testing text"} />
+      </div>
+    </div>
   );
 }
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,11 +2,15 @@ import Link from "next/link"
 
 export function Container({url, title, description}) {
     return (
-        <div className="w-96 h-36 grid gap-2 text-white border border-[#1e1e1e] rounded-lg p-4 hover:bg-[#1e1e1e] duration-300">
-            <Link href={url} target="_blank">
-                <h1 className="text-2xl font-semibold underline underline-offset-4 decoration-1">{title}</h1>
-            </Link>
-                <p className="font-medium">{description}</p>
+        <div className="relative overflow-hidden rounded-lg border border-[#1e1e1e] p-2 text-left hover:bg-[#1e1e1e] duration-300">
+        <div className="flex h-44 flex-col justify-between rounded-md p-6 ">
+        <Link href={url}>
+          <div className="space-y-2 text-white">
+            <h1 className="text-2xl font-semibold">{title}</h1>
+            <p className="font-medium">{description}</p>
+          </div>
+          </Link>
         </div>
+      </div>
     )
 }
